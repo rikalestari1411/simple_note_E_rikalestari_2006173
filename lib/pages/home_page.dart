c91cefa4ffdfc1c055c682853bf0fd5d0b42118b
rikalestari1411 <2006173@itg.ac.id>
2023-06-03 20:37:34 +0700

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:simple_notes/db/database_service.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:simple_notes/extension/my_date.dart';
+
+import '../models/note.dart';
+import '../utils/app_routes.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Simple Notes',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: ValueListenableBuilder(
+        valueListenable: Hive.box(DatabaseService.boxName).listenable(),
+        builder: (context, value, _) {
+          if (value.isEmpty) {
+            return const Center(
+              child: Text("Tidak Ada Data"),
+            );
+          } else {
+            return ListView.separated(
+              itemBuilder: (context, index) {
+                Note tempNote = value.getAt(index);
+                return Dismissible(
+                    key: Key(value.values.toList()[index].key.toString()),
+                    child: NoteCard(
+                      note: tempNote,
+                    ),
+                    onDismissed: (_) async {
+                      await DatabaseService().deleteNote(tempNote);
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text("Data Telah di Hapus"),
+                        ),
+                      );
+                    });
+              },
+              separatorBuilder: (context, index) {
+                return const SizedBox(
+                  height: 8,
+                );
+              },
+              itemCount: value.length,
+            );
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          GoRouter.of(context).pushNamed(AppRoutes.addNote).then((_) {
+            setState(() {});
+          });
+        },
+        child: const Icon(Icons.note_add),
+      ),
+    );
+  }
+}
+
+class NoteCard extends StatelessWidget {
+  const NoteCard({super.key, required this.note});
+  final Note note;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        onTap: () {
+          GoRouter.of(context).pushNamed(
+            AppRoutes.editNote,
+            extra: note,
+          );
+        },
+        title: Text(
+          note.title,
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+        ),
+        subtitle: Text(
+          note.desc,
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+        ),
+        trailing: Text(
+          'Dibuat pada \n ${note.createdAt.formatDate()}',
+          textAlign: TextAlign.start,
+        ),
+      ),
+    );
+  }
+}
